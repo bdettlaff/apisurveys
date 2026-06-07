15e9716a87a529ab262bbfff1143af29dc8f3dc0
detek <bartekdettlaff@interia.pl>
2026-06-07 17:30:33 +0200

    Change API Address

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,11 @@
 export const API_CONFIG = {
-  baseUrl: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'}/api`,
+  baseUrl: `${process.env.NEXT_PUBLIC_API_URL}`,
   endpoints: {
     test: "/test",
   },
 };
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL'
 
 export const apiFetch = (path: string, options?: RequestInit) => {
   return fetch(`${API_URL}${path}`, options)
