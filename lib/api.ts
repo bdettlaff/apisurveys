842596249eccd1f68bb74919dccd36f462b584cd
detek <bartekdettlaff@interia.pl>
2026-06-07 16:39:56 +0200

    Fix in apiFetch

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,12 @@
 export const API_CONFIG = {
-  baseUrl: `${process.env.NEXT_PUBLIC_API_URL`,
+  baseUrl: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'}/api`,
   endpoints: {
     test: "/test",
   },
 };
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'
+
+export const apiFetch = (path: string, options?: RequestInit) => {
+  return fetch(`${API_URL}${path}`, options)
+}
