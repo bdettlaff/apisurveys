27eaadc61031999a0dad42a91d8603b98e3ddc45
detek <bartekdettlaff@interia.pl>
2026-06-07 17:38:55 +0200

    Changes in config

--- lib/api.ts
+++ lib/api.ts
@@ -5,7 +5,7 @@ export const API_CONFIG = {
   },
 };
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const apiFetch = (path: string, options?: RequestInit) => {
   return fetch(`${API_URL}${path}`, options)
